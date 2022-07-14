b37ed4ff5f2c117d2d48edd2ca20a4bb273f6271
Oversword <bionic@oversword.co.uk>
2022-07-14 22:19:23 +0100

    Move file contexts out of filecontext module definition

--- interface/filecontext.js
+++ interface/filecontext.js
@@ -1,211 +1,15 @@
 
 
+
 INTERFACE_SYSTEM
 .define(({
 	tabs,
 	store,
+	dom,
 }) =>
 'filecontext', FACE => {
 	let _context_id = 1
-	const contexts = {
-		level: {
-			make: () => new Promise((resolve, reject) => {
-				const context_id = `${_context_id++}`
-				const canvas = document.createElement('canvas')
-
-				GAME_SYSTEM.define(({  }) =>
-				`dom_${context_id}`, GAME => {
-					GAME.dom = new CanvasDocument(canvas)
-				})
-				.create(({
-					[`dom_${context_id}`]: dom,
-					setup,
-					component_definitions,
-					data,
-					component_data,
-					connection_data,
-					group_data,
-					connection_index,
-					connection_types,
-					connection_render,
-					events,
-					field_element,
-					field_state,
-					group_view,
-					group_operation,
-					interaction,
-					keybindings,
-					fieldinputs,
-					palette_view,
-					play_interaction,
-					play_interaction_rules,
-					remove_operation,
-					component_selection,
-					group_selection,
-					logic_components,
-					physics_components,
-					DOM_events,
-					undo_redo,
-					buttons,
-					component_view,
-					connection_view,
-					connection_component_decorations,
-					connect_interaction,
-					drag_interaction,
-					dragComponents_interaction,
-					drag_group,
-					resize_interaction,
-					rotate_interaction,
-					edit_interaction,
-					edit_component,
-					field_state_view,
-					palette_interface,
-					play_interface,
-					select_interaction,
-					selection,
-					drawing_component,
-					input_components,
-					components,
-					combination,
-					drag_component,
-					field_opacity,
-					group_interface,
-					remove_interface,
-					container_element,
-					edit_interaction_rules,
-					// test_data,
-					data_operations,
-					// data_normalisers,
-					// store,
-				}, { bulkOperations }) =>
-				`edit_level_${context_id}`, GAME => {
-					resolve({
-						id: context_id,
-						canvas,
-						app: GAME
-					})
-				})
-			})
-		},
-		combination: {
-			make: () => new Promise((resolve, reject) => {
-
-				const context_id = `${_context_id++}`
-				const canvas = document.createElement('canvas')
-
-				GAME_SYSTEM.define(({  }) =>
-				`dom_${context_id}`, GAME => {
-					GAME.dom = new CanvasDocument(canvas)
-				})
-				/*
-				.after('data_operations')
-				.define(({ container_element, buttons, pausefieldinputs, pausekeybindings, bulkOperations, component_data, group_data }) =>
-				`edit_combination_${edit_id}`, SUB_GAME => {
-
-					SUB_GAME.buttons.save = {
-						icon(context) {
-							const { y, x, width: w, height: h } = this.getBoundingClientRect()
-
-							context.beginPath()
-							context.moveTo(x+(2*w/16),y+(7*h/16))
-							context.lineTo( x+(0*w/16), y+(11*h/16) )
-							context.lineTo( x+(9*w/16), y+(16*h/16) )
-							context.lineTo( x+(16*w/16), y+(3*h/16) )
-							context.lineTo( x+(12*w/16), y+(0*h/16) )
-							context.lineTo( x+(7*w/16), y+(10*h/16) )
-							context.closePath()
-							context.stroke()
-						},
-						events: {
-							click (event) {
-								const combination = SUB_GAME.bulkOperations.serialize({
-									components: Object.keys(SUB_GAME.component_data.get),
-									groups: Object.values(SUB_GAME.group_data.get).map(({ components }) => Object.keys(components))
-								})
-								GAME.component_definitions[componentDefinition.name].structure = combination
-								rerender()
-								SUB_GAME.container_element.remove()
-								SUB_GAME.pausefieldinputs()
-								SUB_GAME.pausekeybindings()
-								GAME.resumefieldinputs()
-								GAME.resumekeybindings()
-							}
-						},
-						// abovePlayField: true
-					}
-				})
-				*/
-				.create(({
-					[`dom_${context_id}`]: dom,
-					setup,
-					component_definitions,
-					data,
-					component_data,
-					connection_data,
-					group_data,
-					connection_index,
-					connection_types,
-					connection_render,
-					data_operations,
-					events,
-					field_element,
-					field_state,
-					group_view,
-					group_operation,
-					interaction,
-					keybindings,
-					fieldinputs,
-					palette_view,
-					// play_interaction,
-					// play_interaction_rules,
-					// play_interface,
-					remove_operation,
-					component_selection,
-					group_selection,
-					logic_components,
-					physics_components,
-					DOM_events,
-					undo_redo,
-					buttons,
-					component_view,
-					connection_view,
-					connection_component_decorations,
-					connect_interaction,
-					drag_interaction,
-					dragComponents_interaction,
-					drag_group,
-					resize_interaction,
-					rotate_interaction,
-					edit_interaction,
-					edit_component,
-					field_state_view,
-					palette_interface,
-					select_interaction,
-					selection,
-					drawing_component,
-					input_components,
-					components,
-					// combination,
-					drag_component,
-					field_opacity,
-					group_interface,
-					remove_interface,
-					edit_interaction_rules,
-					container_element,
-
-					// [`edit_combination_interface_${edit_id}`]: edit_combination_interface,
-					combination_system_components,
-				}, { bulkOperations }) =>
-				`edit_combination_${context_id}`, GAME => {
-					resolve({
-						id: context_id,
-						canvas,
-						app: GAME
-					})
-				})
-			})
-		}
-	}
+	const contexts = {}
 
 	FACE.tabs.canclose = (index, tab) => {
 		// TODO: Check for unsaved changes?
@@ -231,8 +35,11 @@ INTERFACE_SYSTEM
 				context.path === parDir && context.name === name)
 			if (!context) {
 				const data = FACE.store.get({ path })
+				const constructor = contexts[data.type]
+				if (!constructor)
+					throw new Error(`No context for '${data.type}' is registered`)
 				context = {
-					...(await contexts[data.type].make()),
+					...(await constructor()),
 					name,
 					path: parDir,
 					type: data.type,
@@ -251,6 +58,25 @@ INTERFACE_SYSTEM
 				activeContexts[context.id] = context
 			}
 			return context.id
+		},
+		register(type, call) {
+			if (type in contexts)
+				throw new Error(`Context for '${type}' already registered`)
+
+			contexts[type] = () => new Promise((resolve, reject) => {
+				const context_id = `${_context_id++}`
+				const canvas = FACE.dom.createElement('canvas')
+				const context = {
+					id: context_id,
+					canvas,
+				}
+				call(context, app => resolve({
+					...context,
+					app
+				}))
+			})
+
+			return contexts[type]
 		}
 	}
 
--- interface/index.js
+++ interface/index.js
@@ -5,6 +5,153 @@ INTERFACE_SYSTEM
 	FACE.dom = document
 	FACE.container_element = document.body
 })
+
+.define(({
+	filecontext
+}) =>
+'level_context', FACE => {
+	FACE.filecontext.register('level', (context, done) => {
+
+		GAME_SYSTEM.define(({  }) =>
+		`dom_${context.id}`, GAME => {
+			GAME.dom = new CanvasDocument(context.canvas)
+		})
+		.create(({
+			[`dom_${context.id}`]: dom,
+			setup,
+			component_definitions,
+			data,
+			component_data,
+			connection_data,
+			group_data,
+			connection_index,
+			connection_types,
+			connection_render,
+			events,
+			field_element,
+			field_state,
+			group_view,
+			group_operation,
+			interaction,
+			keybindings,
+			fieldinputs,
+			palette_view,
+			play_interaction,
+			play_interaction_rules,
+			remove_operation,
+			component_selection,
+			group_selection,
+			logic_components,
+			physics_components,
+			DOM_events,
+			undo_redo,
+			buttons,
+			component_view,
+			connection_view,
+			connection_component_decorations,
+			connect_interaction,
+			drag_interaction,
+			dragComponents_interaction,
+			drag_group,
+			resize_interaction,
+			rotate_interaction,
+			edit_interaction,
+			edit_component,
+			field_state_view,
+			palette_interface,
+			play_interface,
+			select_interaction,
+			selection,
+			drawing_component,
+			input_components,
+			components,
+			combination,
+			drag_component,
+			field_opacity,
+			group_interface,
+			remove_interface,
+			container_element,
+			edit_interaction_rules,
+			// test_data,
+			data_operations,
+			// data_normalisers,
+			// store,
+		}, { bulkOperations }) =>
+		`edit_level_${context.id}`, done)
+	})
+})
+.define(({
+	filecontext
+}) =>
+'combination_context', FACE => {
+	FACE.filecontext.register('combination', (context, done) => {
+		GAME_SYSTEM.define(({  }) =>
+		`dom_${context.id}`, GAME => {
+			GAME.dom = new CanvasDocument(context.canvas)
+		})
+		.create(({
+			[`dom_${context.id}`]: dom,
+			setup,
+			component_definitions,
+			data,
+			component_data,
+			connection_data,
+			group_data,
+			connection_index,
+			connection_types,
+			connection_render,
+			data_operations,
+			events,
+			field_element,
+			field_state,
+			group_view,
+			group_operation,
+			interaction,
+			keybindings,
+			fieldinputs,
+			palette_view,
+			// play_interaction,
+			// play_interaction_rules,
+			// play_interface,
+			remove_operation,
+			component_selection,
+			group_selection,
+			logic_components,
+			physics_components,
+			DOM_events,
+			undo_redo,
+			buttons,
+			component_view,
+			connection_view,
+			connection_component_decorations,
+			connect_interaction,
+			drag_interaction,
+			dragComponents_interaction,
+			drag_group,
+			resize_interaction,
+			rotate_interaction,
+			edit_interaction,
+			edit_component,
+			field_state_view,
+			palette_interface,
+			select_interaction,
+			selection,
+			drawing_component,
+			input_components,
+			components,
+			// combination,
+			drag_component,
+			field_opacity,
+			group_interface,
+			remove_interface,
+			edit_interaction_rules,
+			container_element,
+
+			combination_system_components,
+		}, { bulkOperations }) =>
+		`edit_combination_${context.id}`, done)
+	})
+})
 .create(({
 	htma,
 	filecontext,
@@ -16,7 +163,9 @@ INTERFACE_SYSTEM
 	keybindings,
 	events,
 	app_setup,
-	context_operations
+	context_operations,
+	level_context,
+	combination_context
 }) => 'interface', FACE => {
 	Promise.all([
 		FACE.filecontext.add({ path: '/levels/examples/true plat' }),
